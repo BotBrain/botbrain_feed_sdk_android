27722d368461917bbee915b890194b1cbccab2ea
luguoqiang <luguoqiang@firedata.cc>
2017-10-23 11:59:42 +0800

    优化demoUI

---
+++ SampleNews/appCommon/src/main/java/com/peter/newssdkdemo/RefreshStylesFragment.java
@@ -0,0 +1,14 @@
+package com.peter.newssdkdemo;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Description：
+ * Creator: Created by peter.
+ * Date: 2017/10/20.
+ */
+
+public class RefreshStylesFragment extends Fragment {
+
+}
---
+++ SampleNews/appCommon/src/main/java/com/peter/newssdkdemo/RefreshUsingFragment.java
@@ -0,0 +1,13 @@
+package com.peter.newssdkdemo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Description：
+ * Creator: Created by peter.
+ * Date: 2017/10/20.
+ */
+
+public class RefreshUsingFragment extends Fragment {
+
+}
