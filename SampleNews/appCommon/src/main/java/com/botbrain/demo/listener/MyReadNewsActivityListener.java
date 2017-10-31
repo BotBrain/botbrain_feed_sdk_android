95f5b607ae4a36583850ff67c70aefe342d00d89
luguoqiang <luguoqiang@firedata.cc>
2017-10-31 16:58:30 +0800

    连续点击状态栏回调

--- SampleNews/appCommon/src/main/java/com/botbrain/demo/listener/MyReadNewsActivityListener.java
+++ SampleNews/appCommon/src/main/java/com/botbrain/demo/listener/MyReadNewsActivityListener.java
@@ -1,6 +1,7 @@
 package com.botbrain.demo.listener;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.MenuItem;
@@ -22,25 +23,32 @@ public class MyReadNewsActivityListener implements ReadNewsActivityListener {
 
     private Activity mActivity;
 
+    String[] str = new String[]{
+            "设置导航栏背景颜色",
+            "设置导航栏返回图标",
+            "设置导航栏右侧菜单",
+            "设置导航栏标题颜色",
+            "隐藏底部评论框",
+            "设置分享icon",
+            "设置点赞的icon",
+            "隐藏点赞",
+    };
+
     @Override
     public void getReadNewsView(Activity activity, ReadNewsView view) {
         mActivity = activity;
-        /** 设置导航栏背景颜色 */
-        view.setToolBarBackgroundColor(activity.getResources().getColor(R.color.color_e19797));
         /** 设置导航栏返回图标 **/
-        //view.setToolBarNavigationIcon(R.drawable.ic_time_to_leave_black_24dp);
         /** 设置导航栏右侧菜单 **/
-        //activity.setToolBarInflateMenu(R.menu.menu_tip);
         /** 设置导航栏标题颜色 **/
-        view.setToolBarTitleColor(activity.getResources().getColor(R.color.colorPrimary));
+
         /** 隐藏底部评论框 **/
         //view.hideComment();
         /** 设置分享icon **/
-        //view.setShareImageRes(R.drawable.ic_share_black_24dp);
+        //
         /** 设置点赞的icon **/
-        //view.setLikeImageRes(R.drawable.ic_thumb_up_black_24dp, R.drawable.ic_thumb_up_red_24dp);
+        //
         /** 隐藏点赞 **/
-        view.hideLikeView();
+
     }
 
     @Override
@@ -111,6 +119,49 @@ public class MyReadNewsActivityListener implements ReadNewsActivityListener {
     @Override
     public void onDestroy(Activity activity) {
         Log.i(TAG, "onDestroy()");
+    }
+
+    @Override
+    public void onMultipleClick(final Activity activity, final ReadNewsView view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setItems(str, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which) {
+                    case 0:
+                        view.setToolBarBackgroundColor(activity.getResources().getColor(R.color.color_e19797));
+                        break;
+                    // 设置导航栏返回图标
+                    case 1:
+                        view.setToolBarNavigationIcon(R.drawable.ic_time_to_leave_black_24dp);
+                        break;
+                    //设置导航栏右侧菜单
+                    case 2:
+                        view.setToolBarInflateMenu(R.menu.menu_tip);
+                        break;
+                    //设置导航栏标题颜色
+                    case 3:
+                        view.setToolBarTitleColor(activity.getResources().getColor(R.color.colorPrimary));
+                        break;
+                    case 4:
+                        //隐藏底部评论框
+                        view.hideComment();
+                        break;
+                    //设置分享ICON
+                    case 5:
+                        view.setShareImageRes(R.drawable.ic_share_black_24dp);
+                        break;
+                    // 设置点赞的icon
+                    case 6:
+                        view.setLikeImageRes(R.drawable.ic_thumb_up_black_24dp, R.drawable.ic_thumb_up_red_24dp);
+                        break;
+                    // 隐藏点赞
+                    case 7:
+                        view.hideLikeView();
+                }
+            }
+        });
+        builder.show();
     }
 
 }
